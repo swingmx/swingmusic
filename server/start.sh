1802e02179746d63b3556a464f8f3b3d85ef709d
geoffrey45 <geoffreymungai45@gmail.com>
2022-02-13 21:38:31 +0300

    move from pipenv to poetry

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,5 @@
-/home/cwilvx/.local/share/virtualenvs/server-PQNgo_Nv/bin/python manage.py
+# /home/cwilvx/.local/share/virtualenvs/server-PQNgo_Nv/bin/
+
+python manage.py
 
 # gunicorn -b 0.0.0.0:9876 --workers=4 "wsgi:create_app()" --log-level=debug
