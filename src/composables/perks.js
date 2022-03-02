5450a190f3b10df224062181dd5c2f9c5bef3870
geoffrey45 <geoffreymungai45@gmail.com>
2022-03-02 10:20:14 +0300

    update formatSeconds() to correct grammar

--- src/composables/perks.js
+++ src/composables/perks.js
@@ -213,15 +213,37 @@ window.addEventListener("keyup", () => {
 });
 
 function formatSeconds(seconds) {
+  // check if there are arguments
+
   const date = new Date(seconds * 1000);
 
   const hh = date.getUTCHours();
   const mm = date.getUTCMinutes();
   const ss = date.getUTCSeconds();
 
-  const _hh = hh < 10 ? `0${hh}` : hh;
-  const _mm = mm < 10 ? `0${mm}` : mm;
-  const _ss = ss < 10 ? `0${ss}` : ss;
+  let _hh = hh < 10 ? `0${hh}` : hh;
+  let _mm = mm < 10 ? `0${mm}` : mm;
+  let _ss = ss < 10 ? `0${ss}` : ss;
+
+  if (arguments[1]) {
+    if (hh === 1) {
+      _hh = hh + " Hour";
+    } else {
+      _hh = `${hh} Hours`;
+    }
+
+    if (mm === 1) {
+      _mm = mm + " Minute";
+    } else {
+      _mm = `${mm} Minutes`;
+    }
+
+    if (hh > 0) {
+      return `${_hh}, ${_mm}`;
+    } else {
+      return `${_mm}`;
+    }
+  }
 
   if (hh > 0) {
     return `${_hh}:${_mm}:${_ss}`;
