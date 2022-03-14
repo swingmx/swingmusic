d98257de90fa8c9c3f81dcb478c3c2f5191f01d0
geoffrey45 <geoffreymungai45@gmail.com>
2022-03-14 21:04:28 +0300

    convert state.js -> typescript

--- src/composables/state.ts
+++ src/composables/state.ts
@@ -1,64 +1,47 @@
 import { ref } from "@vue/reactivity";
 import { reactive } from "vue";
+import * as i from "../interfaces";
 
 const search_query = ref("");
 
-const queue = ref([
-  {
+const queue = ref(
+  Array<i.Track>({
     title: "Nothing played yet",
     artists: ["... blah blah blah"],
-    image: "http://0.0.0.0:8900/images/defaults/5.webp",
-    _id: {
-      $oid: "",
-    },
-  },
-]);
+    image: "http://0.0.0.0:8900/images/thumbnails/4.webp",
+    trackid: "",
+  })
+);
 
 const folder_song_list = ref([]);
 const folder_list = ref([]);
 
-const current = ref({
+const current = ref(<i.Track>{
   title: "Nothing played yet",
   artists: ["... blah blah blah"],
-  image: "http://0.0.0.0:8900/images/defaults/1.webp",
-  _id: {
-    $oid: "",
-  },
+  image: "http://0.0.0.0:8900/images/thumbnails/4.webp",
+  trackid: "",
 });
 
-const prev = ref({
-  title: "The previous song",
+const prev = ref(<i.Track>{
+  title: "Nothing played yet",
   artists: ["... blah blah blah"],
-  _id: {
-    $oid: "",
-  },
+  image: "http://0.0.0.0:8900/images/thumbnails/4.webp",
+  trackid: "",
 });
 
 const album = reactive({
-  tracklist: [],
-  info: {},
-  artists: [],
+  tracklist: Array<i.Track>(),
+  info: <i.AlbumInfo>{},
+  artists: Array<i.Artist>(),
   bio: "",
 });
 
-const filters = ref([]);
-
-const magic_flag = ref(false);
 const loading = ref(false);
-
 const is_playing = ref(false);
-
 const settings = reactive({
   uri: "http://0.0.0.0:9876",
 });
-
-const tablist = {
-  home: "home",
-  search: "search",
-  queue: "queue",
-};
-
-const current_tab = ref(tablist.home);
 
 export default {
   search_query,
@@ -67,12 +50,8 @@ export default {
   folder_list,
   current,
   prev,
-  filters,
-  magic_flag,
   loading,
   is_playing,
   album,
   settings,
-  current_tab,
-  tablist
 };
