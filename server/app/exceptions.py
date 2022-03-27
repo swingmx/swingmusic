8bb13a109d73ca633f50bb90258ced18adeeff7f
geoffrey45 <geoffreymungai45@gmail.com>
2022-03-27 18:12:20 +0300

    Add Exceptions

---
+++ server/app/exceptions.py
@@ -0,0 +1,14 @@
+class TrackExistsInPlaylist(Exception):
+    """
+    Exception raised when a track is already in a playlist.
+    """
+
+    pass
+
+
+class PlaylistExists(Exception):
+    """
+    Exception raised when a playlist already exists.
+    """
+
+    pass
