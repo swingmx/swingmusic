3b5c62b84db07a20ea102b71f7b8999bd1a205ee
Restyled.io <commits@restyled.io>
2022-04-03 10:45:57 +0000

    Restyled by yapf

--- server/app/api/track.py
+++ server/app/api/track.py
@@ -16,8 +16,7 @@ def send_track_file(trackid):
     """
     try:
         filepath = [
-            file["filepath"]
-            for file in api.PRE_TRACKS
+            file["filepath"] for file in api.PRE_TRACKS
             if file["_id"]["$oid"] == trackid
         ][0]
     except (FileNotFoundError, IndexError):
@@ -32,4 +31,5 @@ def get_sample_track():
     Returns a sample track object.
     """
 
-    return instances.songs_instance.get_song_by_album("Legends Never Die", "Juice WRLD")
+    return instances.songs_instance.get_song_by_album("Legends Never Die",
+                                                      "Juice WRLD")
