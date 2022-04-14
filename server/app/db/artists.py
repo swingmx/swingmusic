f4f003053355ee141ec5799d43ddc77f9b5ba5c7
Restyled.io <commits@restyled.io>
2022-04-14 08:40:40 +0000

    Restyled by black

--- server/app/db/artists.py
+++ server/app/db/artists.py
@@ -18,10 +18,9 @@ class Artists(db.Mongo):
         """
         Inserts an artist into the database.
         """
-        self.collection.update_one(artist_obj, {
-            "$set": artist_obj
-        },
-            upsert=True).upserted_id
+        self.collection.update_one(
+            artist_obj, {"$set": artist_obj}, upsert=True
+        ).upserted_id
 
     def get_all_artists(self) -> list:
         """
