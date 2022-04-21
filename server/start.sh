85ffe985d19f7e71941cc72f02287f6e58d2243f
Restyled.io <commits@restyled.io>
2022-04-21 00:26:55 +0000

    Restyled by shellharden

--- server/start.sh
+++ server/start.sh
@@ -8,4 +8,4 @@
 gpath=$(poetry run which gunicorn)
 
 
-$gpath -b 0.0.0.0:9876 -w 1 --threads=4 "manage:create_app()" #--log-level=debug 
+"$gpath" -b 0.0.0.0:9876 -w 1 --threads=4 "manage:create_app()" #--log-level=debug 
