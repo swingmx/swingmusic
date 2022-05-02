8c363e0d340beac1fe03c79b6789c28fca6defdc
Restyled.io <commits@restyled.io>
2022-05-02 13:10:09 +0000

    Restyled by yapf

--- server/app/functions.py
+++ server/app/functions.py
@@ -57,7 +57,8 @@ def populate():
     albums = []
     folders = set()
 
-    files = helpers.run_fast_scandir(settings.HOME_DIR, [".flac", ".mp3"], full=True)[1]
+    files = helpers.run_fast_scandir(settings.HOME_DIR, [".flac", ".mp3"],
+                                     full=True)[1]
 
     _bar = Bar("Checking files", max=len(files))
     for track in db_tracks:
@@ -102,8 +103,7 @@ def populate():
 
         if albumindex is None:
             track = [
-                track
-                for track in tagged_tracks
+                track for track in tagged_tracks
                 if track["album"] == aa["title"]
                 and track["albumartist"] == aa["artist"]
             ][0]
@@ -126,7 +126,8 @@ def populate():
     _bar = Bar("Creating tracks", max=len(tagged_tracks))
     for track in tagged_tracks:
         try:
-            album_index = albumslib.find_album(track["album"], track["albumartist"])
+            album_index = albumslib.find_album(track["album"],
+                                               track["albumartist"])
             album = api.ALBUMS[album_index]
 
             track["image"] = album.image
@@ -160,11 +161,8 @@ def populate():
     end = time.time()
 
     print(
-        str(datetime.timedelta(seconds=round(end - start)))
-        + " elapsed for "
-        + str(len(files))
-        + " files"
-    )
+        str(datetime.timedelta(seconds=round(end - start))) + " elapsed for " +
+        str(len(files)) + " files")
 
 
 def fetch_image_path(artist: str) -> str or None:
@@ -199,9 +197,8 @@ def fetch_artist_images():
 
     _bar = Bar("Processing images", max=len(artists))
     for artist in artists:
-        file_path = (
-            helpers.app_dir + "/images/artists/" + artist.replace("/", "::") + ".webp"
-        )
+        file_path = (helpers.app_dir + "/images/artists/" +
+                     artist.replace("/", "::") + ".webp")
 
         if not os.path.exists(file_path):
             img_path = fetch_image_path(artist)
@@ -223,8 +220,7 @@ def fetch_album_bio(title: str, albumartist: str):
     Returns the album bio for a given album.
     """
     last_fm_url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key={}&artist={}&album={}&format=json".format(
-        settings.LAST_FM_API_KEY, albumartist, title
-    )
+        settings.LAST_FM_API_KEY, albumartist, title)
 
     try:
         response = requests.get(last_fm_url)
@@ -233,7 +229,8 @@ def fetch_album_bio(title: str, albumartist: str):
         return None
 
     try:
-        bio = data["album"]["wiki"]["summary"].split('<a href="https://www.last.fm/')[0]
+        bio = data["album"]["wiki"]["summary"].split(
+            '<a href="https://www.last.fm/')[0]
     except KeyError:
         bio = None
 
