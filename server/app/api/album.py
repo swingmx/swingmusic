2ca374c51da30b33bde4ab210609d880bcc18896
Restyled.io <commits@restyled.io>
2022-05-04 00:16:38 +0000

    Restyled by reorder-python-imports

--- server/app/api/album.py
+++ server/app/api/album.py
@@ -3,9 +3,14 @@ Contains all the album routes.
 """
 from typing import List
 
-from app import api, functions, helpers, models
-from app.lib import albumslib, trackslib
-from flask import Blueprint, request
+from app import api
+from app import functions
+from app import helpers
+from app import models
+from app.lib import albumslib
+from app.lib import trackslib
+from flask import Blueprint
+from flask import request
 
 album_bp = Blueprint("album", __name__, url_prefix="")
 
