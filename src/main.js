79cbf11af59a5f7c1e93c6c52757d8b2d9df6424
geoffrey45 <geoffreymungai45@gmail.com>
2022-05-25 16:23:59 +0300

    add custom transitions

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import { createPinia } from "pinia";
 import { MotionPlugin } from "@vueuse/motion";
-
+import useCustomTransitions from "./transitions";
 import "../src/assets/css/global.scss";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(MotionPlugin);
+app.use(MotionPlugin, useCustomTransitions);
 
 app.mount("#app");
