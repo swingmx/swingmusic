28110ff88b6a2f565800e607d18f0dade1defb04
geoffrey45 <geoffreymungai45@gmail.com>
2022-06-02 19:35:28 +0300

    move album -> db/mongodb

--- server/app/db/mongodb/__init__.py
+++ server/app/db/mongodb/__init__.py
@@ -1,4 +1,13 @@
+"""
+This module creates and initiliazes a MongoDB instance. It also contains the
+`convert_one()` and `conver_many()` methods for converting MongoDB cursors to Python dicts.
+"""
+
+
+import json
 import pymongo
+from bson import json_util
+
 from app.db import AlbumMethods
 
 
@@ -16,3 +25,33 @@ class MongoAlbums(Mongo, AlbumMethods):
     def __init__(self):
         super(MongoAlbums, self).__init__("ALICE_ALBUMS")
         self.collection = self.db["ALL_ALBUMS"]
+
+
+# ====================================================================== #
+# cursor convertion methods
+
+
+def convert_one(song):
+    """
+    Converts a single mongodb cursor to a json object.
+    """
+    json_song = json.dumps(song, default=json_util.default)
+    loaded_song = json.loads(json_song)
+
+    return loaded_song
+
+
+def convert_many(array):
+    """
+    Converts a list of mongodb cursors to a list of json objects.
+    """
+
+    songs = []
+
+    for song in array:
+        json_song = json.dumps(song, default=json_util.default)
+        loaded_song = json.loads(json_song)
+
+        songs.append(loaded_song)
+
+    return songs
