4301669f7b86e86b2b2942b7d79c20e901dfb19e
Restyled.io <commits@restyled.io>
2022-07-06 14:38:55 +0000

    Restyled by clang-format

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,68 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "@/views/Home.vue";
-import FolderView from "@/views/FolderView.vue";
-import PlaylistView from "@/views/PlaylistView.vue";
-import Playlists from "@/views/Playlists.vue";
-
+import state from "@/composables/state";
+import useAStore from "@/stores/pages/album";
+import useFStore from "@/stores/pages/folder";
+import usePTrackStore from "@/stores/pages/playlist";
+import usePStore from "@/stores/pages/playlists";
 import AlbumsExplorer from "@/views/AlbumsExplorer.vue";
 import AlbumView from "@/views/AlbumView.vue";
-
 import ArtistsExplorer from "@/views/ArtistsExplorer.vue";
+import FolderView from "@/views/FolderView.vue";
+import Home from "@/views/Home.vue";
+import Playlists from "@/views/Playlists.vue";
+import PlaylistView from "@/views/PlaylistView.vue";
 import SettingsView from "@/views/SettingsView.vue";
-
-import usePStore from "@/stores/pages/playlists";
-import usePTrackStore from "@/stores/pages/playlist";
-import useFStore from "@/stores/pages/folder";
-import useAStore from "@/stores/pages/album";
-import state from "@/composables/state";
+import {createRouter, createWebHashHistory} from "vue-router";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path : "/",
+    name : "Home",
+    component : Home,
   },
   {
-    path: "/folder/:path",
-    name: "FolderView",
-    component: FolderView,
-    beforeEnter: async (to) => {
+    path : "/folder/:path",
+    name : "FolderView",
+    component : FolderView,
+    beforeEnter : async (to) => {
       state.loading.value = true;
       await useFStore().fetchAll(to.params.path);
       state.loading.value = false;
     },
   },
   {
-    path: "/folder/",
-    redirect: "/folder/home",
+    path : "/folder/",
+    redirect : "/folder/home",
   },
   {
-    path: "/playlists",
-    name: "Playlists",
-    component: Playlists,
-    beforeEnter: async () => {
+    path : "/playlists",
+    name : "Playlists",
+    component : Playlists,
+    beforeEnter : async () => {
       state.loading.value = true;
       await usePStore().fetchAll();
       state.loading.value = false;
     },
   },
   {
-    path: "/playlist/:pid",
-    name: "PlaylistView",
-    component: PlaylistView,
-    beforeEnter: async (to) => {
+    path : "/playlist/:pid",
+    name : "PlaylistView",
+    component : PlaylistView,
+    beforeEnter : async (to) => {
       state.loading.value = true;
       await usePTrackStore().fetchAll(to.params.pid);
       state.loading.value = false;
     },
   },
   {
-    path: "/albums",
-    name: "AlbumsView",
-    component: AlbumsExplorer,
+    path : "/albums",
+    name : "AlbumsView",
+    component : AlbumsExplorer,
   },
   {
-    path: "/albums/:hash",
-    name: "AlbumView",
-    component: AlbumView,
-    beforeEnter: async (to) => {
+    path : "/albums/:hash",
+    name : "AlbumView",
+    component : AlbumView,
+    beforeEnter : async (to) => {
       state.loading.value = true;
       await useAStore().fetchTracksAndArtists(to.params.hash);
       state.loading.value = false;
@@ -73,24 +70,24 @@ const routes = [
     },
   },
   {
-    path: "/artists",
-    name: "ArtistsView",
-    component: ArtistsExplorer,
+    path : "/artists",
+    name : "ArtistsView",
+    component : ArtistsExplorer,
   },
   {
-    path: "/settings",
-    name: "SettingsView",
-    component: SettingsView,
+    path : "/settings",
+    name : "SettingsView",
+    component : SettingsView,
   },
   {
-    path: "/:pathMatch(.*)",
-    component: () => import("../views/NotFound.vue"),
+    path : "/:pathMatch(.*)",
+    component : () => import("../views/NotFound.vue"),
   },
 ];
 
 const router = createRouter({
-  mode: "hash",
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  mode : "hash",
+  history : createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
