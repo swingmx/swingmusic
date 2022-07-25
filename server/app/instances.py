761095ea2f03173251b52d3f0d0da210355b28da
Restyled.io <commits@restyled.io>
2022-07-25 00:16:42 +0000

    Restyled by reorder-python-imports

--- server/app/instances.py
+++ server/app/instances.py
@@ -1,7 +1,10 @@
 """
 All the MongoDB instances are created here.
 """
-from app.db.mongodb import albums, artists, playlists, tracks
+from app.db.mongodb import albums
+from app.db.mongodb import artists
+from app.db.mongodb import playlists
+from app.db.mongodb import tracks
 from app.settings import DB_TYPE
 
 tracks_instance = None
