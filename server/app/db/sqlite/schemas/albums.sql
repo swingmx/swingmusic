3aab2307fc128cf50a6fcc85b1fa64c65c6c794d
Restyled.io <commits@restyled.io>
2022-07-25 00:16:42 +0000

    Restyled by pg_format

--- server/app/db/sqlite/schemas/albums.sql
+++ server/app/db/sqlite/schemas/albums.sql
@@ -1,11 +1,9 @@
-CREATE TABLE
-  IF NOT EXISTS albums (
-    albumid TEXt PRIMARY KEY,
-    title TEXT,
-    artist TEXT,
-    albumartist TEXT,
-    date TEXT,
-    image TEXT,
-    hash TEXT,
-    colors TEXT
-  )
+CREATE TABLE IF NOT EXISTS albums (
+        albumid TEXt PRIMARY KEY,
+        title TEXT,
+        artist TEXT,
+        albumartist TEXT,
+        date TEXT,
+        image TEXT,
+        HASH TEXT,
+        colors TEXT)
