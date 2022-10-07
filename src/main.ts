0b5e0484a6eb84849ef1d50a4d1e25f1db0db681
geoffrey45 <geoffreymungai45@gmail.com>
2022-10-07 23:14:54 +0300

    rewrite playlist page with  @Akryum/vue-virtual-scroller

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
+// @ts-ignore
+import { RecycleScroller } from "vue-virtual-scroller";
 import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import "./assets/scss/index.scss";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -16,5 +21,6 @@ app.use(pinia);
 app.use(router);
 app.directive("tooltip", vTooltip);
 app.use(autoAnimatePlugin);
+app.component("RecycleScroller", RecycleScroller)
 
 app.mount("#app");
