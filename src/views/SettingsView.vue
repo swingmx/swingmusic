7ef67c646b3665013cba66edfbc53681a9b31d38
geoffrey45 <geoffreymungai45@gmail.com>
2023-01-03 22:17:32 +0300

    fix: scrollbar excess padding on firefox for .content-page

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="settingspage content-page" style="height: 100%; overflow: auto">
+  <div
+    class="settingspage content-page"
+    style="height: 100%; overflow: auto; padding-right: 1.25rem"
+  >
     <Content :current="0" />
   </div>
 </template>
