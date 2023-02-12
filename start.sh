9cb8d372ec6442e783857d4b9c56b71c31ca986b
geoffrey45 <geoffreymungai45@gmail.com>
2023-02-12 23:40:33 +0300

    remove unused files

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-gpath=$(poetry run which gunicorn)
-# pytest=$(poetry run which pytest)
-
-# $pytest # -q
-
-echo "Starting swing"
-"$gpath" -b 0.0.0.0:1970 --threads=2 "manage:create_api()"
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app.api import create_api
-
-if __name__ == '__main__':
-    app = create_api()
-    app.run(debug=False, threaded=True)
