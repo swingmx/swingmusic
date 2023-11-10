5f1bb28f5dbcaf1cf33cf324d8fbde3d06675a48
mungai-njoroge <geoffreymungai45@gmail.com>
2023-11-10 16:27:16 +0300

    remove telemetry from migrations

--- app/migrations/v1_3_0/__init__.py
+++ app/migrations/v1_3_0/__init__.py
@@ -11,8 +11,6 @@ from app.migrations.base import Migration
 from app.settings import Paths
 from app.utils.decorators import coroutine
 from app.utils.hashing import create_hash
-from app.telemetry import Telemetry
-from app.utils.threading import background
 
 # playlists table
 # ---------------
@@ -23,11 +21,6 @@ from app.utils.threading import background
 # 4: last_updated
 # 5: name
 # 6: trackhashes
-
-
-@background
-def send_telemetry():
-    Telemetry.send_app_installed()
 
 
 class RemoveSmallThumbnailFolder(Migration):
@@ -41,7 +34,6 @@ class RemoveSmallThumbnailFolder(Migration):
 
     @staticmethod
     def migrate():
-        send_telemetry()
         thumbs_sm_path = Paths.get_sm_thumb_path()
         thumbs_lg_path = Paths.get_lg_thumb_path()
 
