27a277cc5bc07b9b07432b18e2300f420d675e8c
thecookingsenpai <elmastrococinero@gmail.com>
2024-01-30 15:49:06 +0100

    Organization and usefulness for the final user

---
+++ dev_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# REVIEW Above: bash is way more compatible than other shells
+
+# builds the latest version of the client and server
+
+cd ../swingmusic-client || exit # REVIEW Failsafe exit
+
+yarn build --outDir ../swingmusic/client
+cd ../swingmusic || exit # REVIEW Failsafe exit
+
+# REVIEW Optional cleaning up
+# rm -rf build dist
+
+# REVIEW Install poetry & requirements
+# Build the app
+poetry run python manage.py --build
