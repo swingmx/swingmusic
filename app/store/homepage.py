fe09a8a8ae911f910c27ed3cf89ac94d1c266c98
cwilvx <geoffreymungai45@gmail.com>
2024-10-29 02:10:13 +0300

    fix: ids on homepage store

--- app/store/homepage.py
+++ app/store/homepage.py
@@ -10,9 +10,7 @@ class HomepageStore:
 
     @classmethod
     def set_artist_mixes(cls, mixes: list[Mix], userid: int = 1):
-        print(f"⭐⭐⭐⭐ Setting {len(mixes)} artist mixes")
-        print("mix artists", [mix.title for mix in mixes])
-        idmap = {mix.id: mix for mix in mixes}
+        idmap = {mix.id[1:]: mix for mix in mixes}
         cls.entries["artist_mixes"][userid] = idmap
 
     @classmethod
