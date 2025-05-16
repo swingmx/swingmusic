2c36a993bd7d8e7fdb64ad95db59fbe09b3506f5
michilyy <152296764+michilyy@users.noreply.github.com>
2025-05-16 10:49:30 +0200

    fix import statement

--- app/lib/trackslib.py
+++ app/lib/trackslib.py
@@ -4,8 +4,8 @@ This library contains all the functions related to tracks.
 
 import os
 
-from pydub.pydub import AudioSegment
-from pydub.pydub.silence import detect_leading_silence, detect_silence
+from pydub import AudioSegment
+from pydub.silence import detect_leading_silence, detect_silence
 from app.utils.threading import ProcessWithReturnValue
 
 
