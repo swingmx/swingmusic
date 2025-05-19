1727a5505a8c6a7aade03f04b207770e780c06aa
michilyy <152296764+michilyy@users.noreply.github.com>
2025-05-19 10:53:17 +0200

    `paths.py`: * change from os to pathlib * rework path code

--- src/swingmusic/utils/paths.py
+++ src/swingmusic/utils/paths.py
@@ -1,7 +1,6 @@
-import os
-import sys
-
 from swingmusic.utils.filesystem import get_home_res_path
+from importlib import resources as impresources
+import pathlib
 
 
 def getFlaskOpenApiPath():
@@ -10,9 +9,7 @@ def getFlaskOpenApiPath():
 
     See: https://github.com/luolingchun/flask-openapi3/issues/147
     """
-    site_packages_path = [p for p in sys.path if "site-packages" in p][0]
-
-    return f"{site_packages_path}/flask_openapi3"
+    return impresources.files("flask_openapi3")
 
 
 def getClientFilesExtensions():
@@ -23,9 +20,9 @@ def getClientFilesExtensions():
     client_path = get_home_res_path("client")
 
     extensions = set()
-    for root, dirs, files in os.walk(client_path):
+    for files in pathlib.Path(client_path).iterdir():
         for file in files:
-            ext = file.split(".")[-1]
+            ext = file.suffix
             extensions.add("." + ext)
 
     return extensions
