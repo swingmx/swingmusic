4405e1a240a6222a1af48efae45975903100229a
michilyy <152296764+michilyy@users.noreply.github.com>
2025-07-29 15:48:42 +0200

    folder.py: * strictly use pathlib where possible
    folderlib.py:
    * add missing docstring to function, who really need it.
    
    track.py:
    * refactor some code
    
    folder.py:
    * refactor some more code

--- src/swingmusic/serializers/track.py
+++ src/swingmusic/serializers/track.py
@@ -3,10 +3,17 @@ from dataclasses import asdict
 from swingmusic.models.track import Track
 
 
-def serialize_track(track: Track, to_remove: set = set(), remove_disc=True) -> dict:
+def serialize_track(track: Track, to_remove:set=set(), remove_disc:bool=True) -> dict:
+    """
+    Convert `Track` to dict
+
+    :params track: Track to convert
+    :params to_remove: custom tags which should also be removed from Track.
+    :params remove_disc:
+    """
     album_dict = asdict(track)
-    # is_favorite @property is not included in asdict
-    album_dict["is_favorite"] = track.is_favorite
+    album_keys =  ( key for key in album_dict.keys() if key.startswith(("is_", "_")) )
+    # add all keys from album_dict starting with "is_" or "_"
 
     props = {
         "date",
@@ -27,27 +34,33 @@ def serialize_track(track: Track, to_remove: set = set(), remove_disc=True) -> d
         "lastplayed",
         "playduration",
         "genres",
-    }.union(to_remove)
+        *to_remove,
+        *album_keys,
+    }
 
-    if not remove_disc:
-        props.remove("disc")
-        props.remove("track")
+    if remove_disc:
+        props.add("disc")
+        props.add("track")
 
-    props.update(key for key in album_dict.keys() if key.startswith(("is_", "_")))
-    props.remove("is_favorite")
 
     for key in props:
         album_dict.pop(key, None)
 
-    to_remove_images = ["artists", "albumartists"]
-    for key in to_remove_images:
+    # remove images
+    for key in ["artists", "albumartists"]:
         for artist in album_dict[key]:
             artist.pop("image", None)
+
+    # is_favorite @property is not included in `asdict`
+    album_dict["is_favorite"] = track.is_favorite
 
     return album_dict
 
 
-def serialize_tracks(
-    tracks: list[Track], _remove: set = set(), remove_disc=True
-) -> list[dict]:
+def serialize_tracks(tracks: list[Track], _remove: set = set(), remove_disc=True) -> list[dict]:
+    """
+    wrapper for iterable type with Tracks.
+    convert Tracks to dict and return as list[dict]
+    """
+
     return [serialize_track(t, _remove, remove_disc) for t in tracks]
